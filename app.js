7866dab7c8e99eb506106262c13452829f25cc30
Ramon de Jesus Ortega <ramortega1594@gmail.com>
2022-05-05 20:40:45 -0500

    End HTML structure

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(port, () => {
     console.log(`Corriendo exitosamente en el puerto ${port}`);
