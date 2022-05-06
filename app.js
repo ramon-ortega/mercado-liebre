45e1b7a8bf563ef439c6150528d190843a535527
Ramon de Jesus Ortega <ramortega1594@gmail.com>
2022-05-05 20:08:24 -0500

    Initial Project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('/public'));
+
+app.listen(port, () => {
+    console.log(`Corriendo exitosamente en el puerto ${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
